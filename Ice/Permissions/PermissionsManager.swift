998b270e0c5f8a550a04699aa8ce9c13544cd35f
HomieJohn <LuckyChuckyP123@gmail.com>
2024-07-16 20:53:41 -0400

    Update PermissionsManager.swift

--- Ice/Permissions/PermissionsManager.swift
+++ Ice/Permissions/PermissionsManager.swift
@@ -5,37 +5,6 @@
 
 import Combine
 
-/// A type that manages the permissions of the app.
-class PermissionsManager: ObservableObject {
-    /// A Boolean value that indicates whether the app has been
-    /// granted all permissions.
-    @Published var hasPermission: Bool = false
-
-    let accessibilityPermission = AccessibilityPermission.shared
-    let screenRecordingPermission = ScreenRecordingPermission.shared
-
-    private(set) weak var appState: AppState?
-
-    private var cancellables = Set<AnyCancellable>()
-
-    init(appState: AppState) {
-        self.appState = appState
-        configureCancellables()
-    }
-
-    private func configureCancellables() {
-        var c = Set<AnyCancellable>()
-
-        accessibilityPermission.$hasPermission
-            .combineLatest(screenRecordingPermission.$hasPermission)
-            .sink { [weak self] hasPermission1, hasPermission2 in
-                self?.hasPermission = hasPermission1 && hasPermission2
-            }
-            .store(in: &c)
-
-        cancellables = c
-    }
-
     /// Stops running all permissions checks.
     func stopAllChecks() {
         accessibilityPermission.stopCheck()
